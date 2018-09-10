85f948dbda51f4d8ac8a00a54a6c9c5cb200818f
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-10 03:56:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+function sayHiToGrandma(string) {
+  var cantHear = "I can't hear you!";
+  var yesHear = "YES INDEED!";
+  var love = "I love you, too.";}
+  if (string.toLowerCase(string) === string) {
+    return yesHear;}
+    else if (string.toUpperCase(string) === string) {return cantHear;
+  }
+  else if ("I love you, Grandma." === string) {
+    return love
+  }
+
